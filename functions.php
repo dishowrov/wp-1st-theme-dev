c55cfa9150f6c623b34cb1562a8c5aa9f8d1b3c3
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-02 10:42:19 +0600

    modify

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
 
-function fastier_bootstrapping()
-{
+function fastier_bootstrapping() {
     load_theme_textdomain("fastier");
     add_theme_support("post-thumbnails");
     add_theme_support("title-tag");
 }
 add_action("after_setup_theme", "fastier_bootstrapping");
+
+function fastier_assets() {
+    wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+}
+add_action("wp_enqueue_scripts", "fastier_assets");
+
+?>
